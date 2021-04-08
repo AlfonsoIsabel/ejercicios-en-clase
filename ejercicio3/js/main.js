1da6262d3b81913d6894e61bbd4e65f1c88ca6d1
dayanaCEI <82168957+dayanaCEI@users.noreply.github.com>
2021-04-08 19:49:57 +0200

    Add eje3

---
+++ ejercicio3/js/main.js
@@ -0,0 +1,35 @@
+// dado 3 numeros indicar   el mayor, 
+// y el segundo mayor
+const msjM = document.querySelector(".js-msjM");
+const msjSm = document.querySelector(".js-msjSm");
+
+const a = 370;
+const b = 550;
+const c = 210;
+
+
+if (a > b && a > c) {
+  msjM.innerHTML ="el mayor es el numero" + a;
+  if (b > c) {
+    msjSm.innerHTML = "el segundo mayor es " + b;
+  } else {
+    msjSm.innerHTML = "el segundo mayor es " + c;
+  }
+}
+else if (b > a && b > c) {
+  msjM.innerHTML ="el mayor es " + b;
+  if (a > c) {
+    msjSm.innerHTML = "el segundo mayor es " + a;
+  }
+  else {
+    msjSm.innerHTML = "el segundo mayor es " + c;
+  }
+}
+else {
+  msjM.innerHTML ="el mayor es " + c;
+  if (a > b) {
+    msjSm.innerHTML = "el segundo mayor es " + a;
+  } else {
+    msjSm.innerHTML = "el segundo mayor es " + b;
+  }
+}
